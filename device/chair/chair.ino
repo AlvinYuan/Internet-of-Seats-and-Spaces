eb22591896fbc340476e530008ad5e14c399437d
Alvin Yuan <alvin.yuan92@gmail.com>
2015-03-11 22:52:48 -0700

    Initial commit. Device code for assignment 6: basic posting to asbase.

---
+++ device/chair/chair.ino
@@ -0,0 +1,82 @@
+// Enums
+enum FreeTakenState {
+  FREE,
+  TAKEN,
+};
+
+// Constants
+const int loadSensorPin = A0;
+const int ledPin = 13;
+const int loadSensorThreshold = 900;
+const long minStateChangePeriod = 5000; // milliseconds
+
+// State
+FreeTakenState chairState = TAKEN;
+long lastStateChangeMillis = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600); // for debugging and communicating with Processing
+}
+
+void loop() {
+  // Determine new state
+  int loadSensorValue = analogRead(loadSensorPin);
+  FreeTakenState newChairState = loadSensorValue > loadSensorThreshold ? TAKEN : FREE;
+  long currentMillis = millis();
+
+  // Only do something if the state changes.
+  if (   newChairState != chairState 
+      && currentMillis - lastStateChangeMillis > minStateChangePeriod) {
+    // Update state
+    chairState = newChairState;
+    lastStateChangeMillis = currentMillis;
+    stateUpdate();
+  }
+}
+
+void stateUpdate() {
+  String actor = "{\"displayName\":\"Unknown\",\"objectType\":\"person\"}"; // default Unknown person
+  String verb;
+  switch(chairState) {
+    case FREE:
+      digitalWrite(ledPin, HIGH);
+      verb = "\"leave\"";
+      break;
+    case TAKEN:
+      digitalWrite(ledPin, LOW);
+      verb = "\"checkin\"";
+      break;      
+  }
+  String published = "\"2011-02-10T15:04:55Z\""; // TODO: actually determine time 
+  String postData = "{\"actor\":" + actor + 
+                     ",\"verb\":" + verb + 
+                   ",\"object\":" + selfObjectString() + 
+                ",\"published\":" + published +
+                    "}";
+  Serial.println(postData);
+}
+
+String selfObjectString() {
+  String objectType = "\"place\"";
+  int id = 1;
+  String idString = "\"http://example.org/berkeley/southhall/202/chair/" + String(id) + "\"";
+  String displayName = "\"Chair at 202 South Hall, UC Berkeley\"";
+  String descriptor_tags = "[\"chair\",\"rolling\"]";
+  return "{\"objectType\":" + objectType + 
+                 ",\"id\":" + idString + 
+        ",\"displayName\":" + displayName + 
+    ",\"descriptor_tags\":" + descriptor_tags + 
+           ",\"position\":" + selfPositionString() +
+         "}";
+}
+
+String selfPositionString() {
+  String latitude = "34.34";
+  String longitude = "-127.23";
+  String altitude = "100.05";
+  return "{\"latitude\":" + latitude +
+        ",\"longitude\":" + longitude + 
+         ",\"altitude\":" + altitude +
+         "}";
+}
---
+++ device/processing_wifi/processing_wifi.pde
@@ -0,0 +1,61 @@
+import processing.serial.*;
+import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// https://learn.sparkfun.com/tutorials/connecting-arduino-to-processing
+
+// State
+Serial myPort;  // Create object from Serial class
+
+void setup() {
+  println(Serial.list());
+  String portName = Serial.list()[0];
+  myPort = new Serial(this, portName, 9600); 
+}
+
+void draw()
+{
+  if ( myPort.available() > 0) 
+  {
+    String activityString = myPort.readStringUntil('\n');
+    if (activityString == null) {
+      return;
+    }
+    println(activityString);
+    postToASBase(activityString);
+  }
+}
+
+// http://stackoverflow.com/questions/4205980/java-sending-http-parameters-via-post-method-easily
+void postToASBase(String activityJsonString) {
+  try {
+    URL asBaseURL = new URL("http://russet.ischool.berkeley.edu:8080/activities");
+    HttpURLConnection asBaseConn = (HttpURLConnection) asBaseURL.openConnection();
+    byte[] postData = activityJsonString.getBytes("UTF-8");
+
+    asBaseConn.setDoOutput(true);
+    asBaseConn.setDoInput(true);
+    asBaseConn.setRequestMethod("POST");
+    asBaseConn.setRequestProperty( "Content-Type", "application/stream+json");
+    asBaseConn.setRequestProperty("Content-Length", String.valueOf(postData.length));
+    asBaseConn.getOutputStream().write(postData);
+  
+    BufferedReader in = new BufferedReader(
+            new InputStreamReader(asBaseConn.getInputStream()));
+    String inputLine;
+    StringBuffer response = new StringBuffer();
+  
+    while ((inputLine = in.readLine()) != null) {
+        response.append(inputLine);
+    }
+    in.close();
+  
+    //print result
+    println(response.toString());
+  } catch (Exception e) {
+    println(e.getMessage());
+    e.printStackTrace();
+  }
+}
+
