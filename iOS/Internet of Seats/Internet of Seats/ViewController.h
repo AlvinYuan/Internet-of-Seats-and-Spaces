69e56fb76349a5e626f358981ecfc91d43147e18
Pi-Tan Hu <lacontrazania@gmail.com>
2015-03-20 13:58:50 -0700

    iOS - initial check-in

---
+++ iOS/Internet of Seats/Internet of Seats/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Internet of Seats
+//
+//  Created by Pi-Tan Hu on 3/20/15.
+//  Copyright (c) 2015 LaContra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)sendRequestToASBase:(id)sender;
+
+@end
+
