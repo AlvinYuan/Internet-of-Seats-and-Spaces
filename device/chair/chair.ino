fd4f68c6f9b6e35fcfefe3ae79805e91346ccff5
Alvin Yuan <alvin.yuan92@gmail.com>
2015-03-21 03:14:09 -0700

    wifi module support.

--- device/chair/chair.ino
+++ device/chair/chair.ino
@@ -13,8 +13,6 @@ enum FreeTakenState {
 // Pins
 const int START_BUTTON_PIN = 8;
 const int LOAD_SENSOR_PIN = A0;
-const int LED_PIN = 13;
-
 
 // CC3000
 const bool USE_CC3000 = true; // if false, use processing_wifi
@@ -37,8 +35,9 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
                                    // received before closing the connection.  If you know the server
                                    // you're accessing is quick to respond, you can reduce this value.
                                    
-#define WEBSITE      "www.adafruit.com"
-#define WEBPAGE      "/testwifi/index.html"
+#define ACTIVITY_STREAM_WEBSITE      "russet.ISchool.Berkeley.EDU"
+#define ACTIVITY_STREAM_WEBSITE_PORT 8080
+#define ADAFRUIT_WEBSITE "www.adafruit.com"
 
 // Constants
 const int loadSensorThreshold = 900;
@@ -48,11 +47,21 @@ const long minStateChangePeriod = 5000; // milliseconds
 FreeTakenState chairState = TAKEN;
 long lastStateChangeMillis = 0;
 
+uint32_t activityStreamServerIp = 0;
+uint32_t adafruitIp = 0; // for testing connection
+
+// Object Information
+const char *objectType = "\"place\"";
+const char *id = "\"http://example.org/berkeley/southhall/202/chair/1\"";
+char *displayName = "\"Chair at 202 South Hall, UC Berkeley\"";
+char *descriptor_tags = "[\"chair\",\"rolling\"]";
+char* latitude = "34.34";
+char* longitude = "-127.23";
+char* altitude = "100.05";
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
   Serial.begin(115200);
-  
+
   Serial.println("Press the Start Button!");
   while (digitalRead(START_BUTTON_PIN) == LOW); // block until start button pressed
   if (USE_CC3000) {
@@ -63,7 +72,6 @@ void setup() {
 void loop() {
   // Determine new state
   int loadSensorValue = analogRead(LOAD_SENSOR_PIN);
-//  Serial.println(loadSensorValue);
   FreeTakenState newChairState = loadSensorValue > loadSensorThreshold ? FREE : TAKEN;
   long currentMillis = millis();
 
@@ -78,59 +86,118 @@ void loop() {
 }
 
 void stateUpdate() {
-  String actor = "{\"displayName\":\"Unknown\",\"objectType\":\"person\"}"; // default Unknown person
-  String verb;
+  char* actor = "{\"displayName\":\"Unknown\",\"objectType\":\"person\"}"; // default Unknown person
+
+  char* verb;
   switch(chairState) {
     case FREE:
-      digitalWrite(LED_PIN, HIGH);
       verb = "\"leave\"";
       break;
     case TAKEN:
-      digitalWrite(LED_PIN, LOW);
       verb = "\"checkin\"";
       break;      
   }
-  String published = "\"2011-02-10T15:04:55Z\""; // TODO: actually determine time
+  char* published = "\"2011-02-10T15:04:55Z\""; // TODO: actually determine time
   if (USE_CC3000) {
+    postActivityToCC3000(actor, verb, published);
   } else {
     postActivityToSerial(actor, verb, published);
   }
 }
 
-void postActivityToSerial(String actor, String verb, String published) {
-  Serial.print("{\"actor\":");
-  Serial.print(actor);
-  Serial.print(",\"verb\":");
-  Serial.print(verb);
-  Serial.print(",\"object\":");
-  Serial.print(selfObjectString());
-  Serial.print(",\"published\":");
-  Serial.print(published);
-  Serial.println("}");
+void postActivityToCC3000(char* actor, char* verb, char* published) {
+  // TODO: figure out a cleaner way to specify this? Beware of memory limits
+  int contentLength = 
+    1 + 
+    1 + 5 + 2 + strlen(actor) + 
+    2 + 4 + 2 + strlen(verb) +
+    2 + 6 + 2 +
+      1 +
+      1 + 10 + 2 + strlen(objectType) +
+      2 + 2 + 2 + strlen(id) +
+      2 + 11 + 2 + strlen(displayName) +
+      2 + 15 + 2 + strlen(descriptor_tags) +
+      2 + 8 + 2 +
+        1 +
+        1 + 8 + 2 + strlen(latitude) +
+        2 + 9 + 2 + strlen(longitude) +
+        2 + 8 + 2 + strlen(altitude) +
+        1 +
+      1 +
+     2 + 9 + 2 + strlen(published) +
+     1;
+  String contentLengthString = String(contentLength);
+  int contentLengthCLength = contentLengthString.length() + 1;
+  char contentLengthC[contentLengthCLength];
+  contentLengthString.toCharArray(contentLengthC, contentLengthCLength);
+  Serial.println(cc3000.checkConnected());
+  Adafruit_CC3000_Client www = cc3000.connectTCP(activityStreamServerIp, ACTIVITY_STREAM_WEBSITE_PORT);
+  if (www.connected()) {
+    www.fastrprint(F("POST /activities HTTP/1.1\r\n"));
+    www.fastrprint(F("Host: ")); www.fastrprint(F(ACTIVITY_STREAM_WEBSITE)); www.fastrprint(F("\r\n"));
+    www.fastrprint(F("Content-Type: application/stream+json\r\n"));
+    www.fastrprint(F("Content-Length: ")); www.fastrprint(contentLengthC); www.fastrprint(F("\r\n"));
+    www.fastrprint(F("\r\n"));
+    // Activity JSON
+    www.fastrprint(F("{"));
+    www.fastrprint(F("\"actor\":")); www.fastrprint(actor);
+    www.fastrprint(F(",\"verb\":")); www.fastrprint(verb);
+    www.fastrprint(F(",\"object\":"));
+      // TODO: figure out a cleaner way to specify this? Beware of memory limits
+      www.fastrprint(F("{"));
+      www.fastrprint(F("\"objectType\":")); www.fastrprint(objectType);
+      www.fastrprint(F(",\"id\":")); www.fastrprint(id);
+      www.fastrprint(F(",\"displayName\":")); www.fastrprint(displayName);
+      www.fastrprint(F(",\"descriptor_tags\":")); www.fastrprint(descriptor_tags);
+      www.fastrprint(F(",\"position\":"));
+        www.fastrprint(F("{"));
+        www.fastrprint(F("\"latitude\":")); www.fastrprint(latitude);
+        www.fastrprint(F(",\"longitude\":")); www.fastrprint(longitude);
+        www.fastrprint(F(",\"altitude\":")); www.fastrprint(altitude);
+        www.fastrprint(F("}"));
+      www.fastrprint(F("}"));
+    www.fastrprint(F(",\"published\":")); www.fastrprint(published);
+    www.fastrprint(F("}"));
+    www.println();
+
+    /* Read data until either the connection is closed, or the idle timeout is reached. */ 
+    unsigned long lastRead = millis();
+    while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
+      while (www.available()) {
+        char c = www.read();
+        Serial.print(c);
+        lastRead = millis();
+      }
+    }
+    www.close();
+    Serial.println();
+  } else {
+    Serial.println(F("Connection failed"));    
+    return;
+  }
 }
 
-String selfObjectString() {
-  String objectType = "\"place\"";
-  int id = 1;
-  String idString = "\"http://example.org/berkeley/southhall/202/chair/" + String(id) + "\"";
-  String displayName = "\"Chair at 202 South Hall, UC Berkeley\"";
-  String descriptor_tags = "[\"chair\",\"rolling\"]";
-  return "{\"objectType\":" + objectType + 
-                 ",\"id\":" + idString + 
-        ",\"displayName\":" + displayName + 
-    ",\"descriptor_tags\":" + descriptor_tags + 
-           ",\"position\":" + selfPositionString() +
-         "}";
-}
-
-String selfPositionString() {
-  String latitude = "34.34";
-  String longitude = "-127.23";
-  String altitude = "100.05";
-  return "{\"latitude\":" + latitude +
-        ",\"longitude\":" + longitude + 
-         ",\"altitude\":" + altitude +
-         "}";
+void postActivityToSerial(char* actor, char* verb, char* published) {
+  // TODO: figure out a cleaner way to specify this? Beware of memory limits
+  Serial.print(F("{"));
+  Serial.print(F("\"actor\":")); Serial.print(actor);
+  Serial.print(F(",\"verb\":")); Serial.print(verb);
+  Serial.print(F(",\"object\":"));
+    Serial.print(F("{"));
+    Serial.print(F("\"objectType\":")); Serial.print(objectType);
+    Serial.print(F(",\"id\":")); Serial.print(id);
+    Serial.print(F(",\"displayName\":")); Serial.print(displayName);
+    Serial.print(F(",\"descriptor_tags\":")); Serial.print(descriptor_tags);
+    Serial.print(F(",\"position\":"));
+      Serial.print(F("{"));
+      Serial.print(F("\"latitude\":")); Serial.print(latitude);
+      Serial.print(F(",\"longitude\":")); Serial.print(longitude);
+      Serial.print(F(",\"altitude\":")); Serial.print(altitude);
+      Serial.print(F("}"));
+    Serial.print(F("}"));
+  Serial.print(F(",\"published\":")); Serial.print(published);
+  Serial.print(F("}"));
+  Serial.println();
 }
 
 /***************************************************************************
@@ -170,27 +237,43 @@ void initializeConnection() {
     delay(1000);
   }
 
-  uint32_t ip = 0;
   // Try looking up the website's IP address
-  Serial.print(WEBSITE); Serial.print(F(" -> "));
-  while (ip == 0) {
-    if (! cc3000.getHostByName(WEBSITE, &ip)) {
+  Serial.print(ACTIVITY_STREAM_WEBSITE); Serial.print(F(" -> "));
+  while (activityStreamServerIp == 0) {
+    if (! cc3000.getHostByName(ACTIVITY_STREAM_WEBSITE, &activityStreamServerIp)) {
+      Serial.println(F("Couldn't resolve!"));
+    }
+    delay(500);
+  }
+  cc3000.printIPdotsRev(activityStreamServerIp);
+  Serial.println();
+  
+  testConnect();
+
+}
+
+void testConnect() {
+  // Try looking up adafruit IP address
+  Serial.print(ADAFRUIT_WEBSITE); Serial.print(F(" -> "));
+  while (adafruitIp == 0) {
+    if (! cc3000.getHostByName(ADAFRUIT_WEBSITE, &adafruitIp)) {
       Serial.println(F("Couldn't resolve!"));
     }
     delay(500);
   }
 
-  cc3000.printIPdotsRev(ip);
+  cc3000.printIPdotsRev(adafruitIp);
+  Serial.println();
 
   /* Try connecting to the website.
      Note: HTTP/1.1 protocol is used to keep the server from closing the connection before all data is read.
   */
-  Adafruit_CC3000_Client www = cc3000.connectTCP(ip, 80);
+  Adafruit_CC3000_Client www = cc3000.connectTCP(adafruitIp, 80);
   if (www.connected()) {
     www.fastrprint(F("GET "));
-    www.fastrprint(WEBPAGE);
+    www.fastrprint("/testwifi/index.html");
     www.fastrprint(F(" HTTP/1.1\r\n"));
-    www.fastrprint(F("Host: ")); www.fastrprint(WEBSITE); www.fastrprint(F("\r\n"));
+    www.fastrprint(F("Host: ")); www.fastrprint(ADAFRUIT_WEBSITE); www.fastrprint(F("\r\n"));
     www.fastrprint(F("\r\n"));
     www.println();
   } else {
@@ -198,24 +281,19 @@ void initializeConnection() {
     return;
   }
 
-  Serial.println(F("-------------------------------------"));
-  
   /* Read data until either the connection is closed, or the idle timeout is reached. */ 
   unsigned long lastRead = millis();
+  int count = 0;
   while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
     while (www.available()) {
       char c = www.read();
       Serial.print(c);
+      count = count + 1;
       lastRead = millis();
     }
   }
   www.close();
-  Serial.println(F("-------------------------------------"));
-  
-  /* You need to make sure to clean up after yourself or the CC3000 can freak out */
-  /* the next time your try to connect ... */
-  Serial.println(F("\n\nDisconnecting"));
-  cc3000.disconnect();
+  Serial.println(count);
 }
 
 /**************************************************************************/
