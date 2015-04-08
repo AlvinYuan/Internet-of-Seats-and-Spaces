6c73f6c1cdd041cfc15451a3fecd68c4f22f911f
Pi-Tan Hu <lacontrazania@gmail.com>
2015-04-07 21:18:15 -0700

    register device method for push noti

--- seatsServer/seatsServer/settings.py
+++ seatsServer/seatsServer/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'push_notifications',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -105,3 +106,10 @@ STATICFILES_DIRS = (
 
 # https://docs.djangoproject.com/en/dev/ref/contrib/sites/#enabling-the-sites-framework
 SITE_ID = 1
+
+# Push notifications certificate location
+PUSH_NOTIFICATIONS_SETTINGS = {
+        "APNS_CERTIFICATE": os.path.join(BASE_DIR, 'resources' ,'iotseats_ck.pem'),
+}
+
+SOUTH_MIGRATION_MODULES = {"push_notifications": "push_notifications.south_migrations"}
--- seatsServer/seatsServer/urls.py
+++ seatsServer/seatsServer/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import reservations.views
+import phoneNotifications.views
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,4 +12,10 @@ urlpatterns = patterns('',
     url(r'^new_request/', reservations.views.new_request, name='new_request'),
     url(r'^create_subscriber/', reservations.views.create_subscriber, name='create_subscriber'),
     url(r'^create_reservation_subscription/', reservations.views.create_reservation_subscription, name='create_reservation_subscription'),
+
+    url(r'^register_device/', phoneNotifications.views.register_device, name='register_device'),
+    url(r'^reservation_result/', phoneNotifications.views.reservation_result, name='reservation_result'),
+    url(r'^create_phone_notifications_subscriber/', phoneNotifications.views.create_phone_notifications_subscriber, name='create_phone_notifications_subscriber'),
+    url(r'^create_deny_reservation_subscription/', phoneNotifications.views.create_deny_reservation_subscription, name='create_deny_reservation_subscription'),
+    url(r'^create_approve_reservation_subscription/', phoneNotifications.views.create_approve_reservation_subscription, name='create_approve_reservation_subscription'),   
 )
